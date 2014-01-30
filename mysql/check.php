2930a9e0717667f10a653ea8e03b139f579956f4
Brett O'Donnell <cornernote@gmail.com>
2014-01-30 23:05:32 +1030

    Update check.php

--- mysql/check.php
+++ mysql/check.php
@@ -19,22 +19,50 @@ $path = '/backup/mysql';
 
 // do some checks
 $errors = array();
+$warnings = array();
 
-// check file count
-$count = count(glob($path . '/*'));
+// check daily file count
+$count = count(glob($path . '/' . date('Y-m-d') . '/*'));
 if (!$count) {
     $errors[] = 'backup has no files';
 }
 
-// compile a message
-$message = "files:{$count}";
+// check file age in s3
+ob_start();
+system('s3cmd ls s3://factoryfast-backup/mysql/daily/');
+$s3List = explode("\n", trim(ob_get_clean()));
+foreach ($s3List as $s3File) {
+    $s3File = explode(' ', preg_replace('/\s+/', ' ',$s3File));
+    if (strtotime($s3File[0]) < strtotime('yesterday')) {
+        $warnings[] = $s3File[3] . ' is too old (' . $s3File[0] . ')';
+    }
+}
+
+// check file count in s3
+if (count($s3List) != $count) {
+    $warnings[] = 's3 daily count does not match local count';
+}
+
+// check weekly files in s3
+ob_start();
+system('s3cmd ls s3://factoryfast-backup/mysql/weekly/' . date('Y-m-d', strtotime('last sunday')) . '/ | wc -l');
+$s3CountWeekly = ob_get_clean();
+if (!$s3CountWeekly) {
+    $warnings[] = 's3 weekly backup has no files';
+}
 
 // some errors
 if ($errors) {
-    echo 'CRITICAL: ' . implode(', ', $errors) . ' - ' . $message;
+    echo 'CRITICAL: ' . implode(', ', $errors);
     die(CRITICAL);
 }
 
+// some warnings
+if ($warnings) {
+    echo 'WARNING: ' . implode(', ', $warnings);
+    die(WARNING);
+}
+
 // checks pass, sweet!
-echo 'OK: ' . $message;
+echo 'OK: files:' . $count;
 die(OK);
