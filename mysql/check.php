3fe774b6b4847dbc246249edd7392a51d30291be
Brett O'Donnell <cornernote@gmail.com>
2014-02-01 19:54:49 +1030

    Update check.php

--- mysql/check.php
+++ mysql/check.php
@@ -46,7 +46,7 @@ foreach ($s3List as $s3File) {
     $localFilename = $dailyPath . substr($s3File[4], strlen($s3BucketDaily));
     // compare filesize
     if (filesize($localFilename) != $s3File[2]) {
-        $warnings[] = $s3File[4] . ' filezise does not match s3 (' . $s3File[3] . ')';
+        $warnings[] = $s3File[4] . ' filezise does not match s3 (' . $s3File[2] . ')';
     }
     // compare md5
     elseif (md5_file($localFilename) != $s3File[3]) {
