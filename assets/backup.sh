a375bfd13ff945b5e19091d09044320be5fca245
Brett O'Donnell <cornernote@gmail.com>
2014-03-10 19:56:06 +1030

    Update backup.sh

--- assets/backup.sh
+++ assets/backup.sh
@@ -10,12 +10,12 @@
 
 # config
 SOURCE=/home/you/assets/
-BACKUPDIR=/backup/assets/
+DESTINATION=/backup/assets/
 BACKUPDAYS=14
-BACKUPNAME=/backup/assets-`date +"%Y-%m-%d"`.tgz
 S3BUCKET=s3://bucket-name/assets/
 FULLBACKUPDAY=Sun
 DAILYNAME=/backup/assets.tgz
+WEEKLYNAME=${S3BUCKET}weekly/assets-`date +"%Y-%m-%d"`.tgz
 
 # binary paths
 TAR=`which tar`
@@ -23,35 +23,27 @@ FIND=`which find`
 S3CMD=`which s3cmd`
 RDIFFBACKUP=`which rdiff-backup`
 RM=/bin/rm
+CP=/bin/cp
 
 # create daily increment
-${RDIFFBACKUP} ${SOURCE} ${BACKUPDIR}
+${RDIFFBACKUP} ${SOURCE} ${DESTINATION}
 
 # remove old increments
-${RDIFFBACKUP} -v2 --force --remove-older-than ${BACKUPDAYS}D ${BACKUPDIR}
+${RDIFFBACKUP} -v2 --force --remove-older-than ${BACKUPDAYS}D ${DESTINATION}
 
 # delete compressed backup
 ${RM} -f ${DAILYNAME}
 
 # compress latest daily backup
-${TAR} cfzP ${DAILYNAME} ${BACKUPDIR}
+${TAR} cfzP ${DAILYNAME} ${DESTINATION}
 
 # upload changed files to s3
 ${S3CMD} put ${DAILYNAME} ${S3BUCKET}
 
-# upload changed files to s3
-#${S3CMD} sync -r --delete-removed ${BACKUPDIR} ${S3BUCKET}daily/
-
 # check if we do a full remote backup today
 if [[ `date '+%a'` == ${FULLBACKUPDAY} ]]; then
 
-	# compress latest daily backup
-	${TAR} cfzP ${BACKUPNAME} ${BACKUPDIR}
-
 	# upload compressed backup to s3
-	${S3CMD} put ${BACKUPNAME} ${S3BUCKET}weekly/
-
-	# delete compressed backup
-	${RM} -f ${BACKUPNAME}
+	${S3CMD} put ${DAILYNAME} ${WEEKLYNAME}
 
 fi
